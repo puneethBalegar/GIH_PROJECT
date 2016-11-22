da7764018b9bd0bd1c722ecef41bf2d5222a63af
puneethBalegar <puneethbalegar@yahoo.com>
2016-11-22 20:46:49 +0530

    Update travis-build-upload.sh

--- travis-build-upload.sh
+++ travis-build-upload.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 git clone -b GIH_PROJECT_BRANCH --single-branch https://puneethbalegar:d45ac1e7f19e5e63161437a1e5018af7f137b1111@github.com/puneethBalegar/GIH_PROJECT.git
+mv test.txt ${TRAVIS_BUILD_ID}".txt"
+mv ${TRAVIS_BUILD_ID}".txt" GIH_PROJECT
 cd GIH_PROJECT
 git branch 
-git config user.name "puneethbalegar"
+git config user.name "puneethBalegar"
 git config user.email "puneethbalegar@yahoo.com"
 git config --global push.default simple
 git add .
 git status 
 git commit -m "New BUILD PUSH"
-git push https://puneethbalegar:d45ac1e7f19e5e63161437a1e5018af7f137b1111@github.com/puneethBalegar/GIH_PROJECT.git
+git push https://puneethBalegar:d45ac1e7f19e5e63161437a1e5018af7f137b111@github.com/puneethBalegar/GIH_PROJECT.git

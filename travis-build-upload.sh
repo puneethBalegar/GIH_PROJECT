2e67dfad3cd37a1bdb990e55af7c5c77fba857b9
puneethBalegar <puneethbalegar@yahoo.com>
2016-11-22 15:48:02 +0530

    Update travis-build-upload.sh

--- travis-build-upload.sh
+++ travis-build-upload.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone -b GIH_PROJECT_BRANCH --single-branch https://puneethbalegar:d45ac1e7f19e5e63161437a1e5018af7f137b1111@github.com/puneethBalegar/GIH_PROJECT.git
-cd GIH_PROJECT_BRANCH
 git branch 
 git config user.name "puneethbalegar"
 git config user.email "puneethbalegar@yahoo.com"
